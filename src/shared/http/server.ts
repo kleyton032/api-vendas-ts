353f4af42887b6e2e9c03fa08e6d0e87ec171d9b
Kleyton João Ribeiro Bomfim <kleyton.bomfim@FAV.COM.BR>
2021-08-06 17:49:11 -0300

    created class errors intercepd

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('DEV')
+import express, { application } from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, ()=>{
+    console.log('Server started on port 3333!');
+})
