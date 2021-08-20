91ff2ec755e140d1f61faf43c4fd1b4124cb8b0f
Kleyton João <kleyton.joao@gmail.com>
2021-08-20 17:44:37 -0300

    implemented validation celebrate/Joi in the module products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express, { application, NextFunction, Request, response, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes/index';
+import { errors } from 'celebrate';
+import 'express-async-errors';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 
@@ -12,20 +14,26 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-    if(error instanceof AppError){
-        return res.status(error.statusCode).json({
-            status: 'error',
-            message: error.message
-        })
-    }
+app.use(errors());
 
-    return response.status(500).json({
+app.use(
+    (error: Error, request: Request, response: Response, next: NextFunction) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+  
+      console.log(error);
+  
+      return response.status(500).json({
         status: 'error',
         message: 'Internal server error',
-    })
-})
+      });
+    },
+  );
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
     console.log('Server started on port 3333!');
 })
