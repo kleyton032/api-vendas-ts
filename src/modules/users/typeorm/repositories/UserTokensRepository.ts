b3d83223f555f7e734493b385d32e5fe50027f60
Kleyton João <kleyton.joao@gmail.com>
2022-06-07 10:49:32 -0300

    create UserTokenRepository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,25 @@
+import { EntityRepository, Repository } from "typeorm";
+import User from '../entities/User';
+import UserToken from "../entities/UserToken";
+
+@EntityRepository(UserToken)
+export class UserTokenRepository extends Repository<UserToken> {
+    public async findByToken(token: string): Promise<UserToken | undefined> {
+        const userToken = await this.findOne({
+            where: {
+                token,
+            },
+        })
+
+        return userToken;
+    }
+
+    public async generate(user_id: string): Promise<UserToken | undefined> {
+        const token = await this.create({
+            user_id,
+        })
+
+        return token;
+    }
+
+}
