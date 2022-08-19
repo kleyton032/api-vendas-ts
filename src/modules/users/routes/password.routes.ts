275387b41578ea876fb6436cc19e4f2db734fc5d
Kleyton João <kleyton.joao@gmail.com>
2022-08-19 16:06:26 -0300

    create config class fakemail with nodemailer

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
-import SessionsController from '../controllers/SessionsController';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
  const passwordRouter = Router();
 
  const forgotPasswordController = new ForgotPasswordController();
+ const resetPasswordController = new ResetPasswordController();
 
  passwordRouter.post('/forgot', celebrate({
      [Segments.BODY]: {
@@ -14,5 +15,15 @@ import ForgotPasswordController from '../controllers/ForgotPasswordController';
  }),
  forgotPasswordController.create,
  )
+
+ passwordRouter.post('/reset', celebrate({
+    [Segments.BODY]: {
+        token: Joi.string().uuid().required(),
+        password: Joi.string().required(),
+        password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+}),
+resetPasswordController.create,
+)
 
  export default passwordRouter;
